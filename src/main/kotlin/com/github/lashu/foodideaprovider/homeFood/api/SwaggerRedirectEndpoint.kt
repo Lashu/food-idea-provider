d92f3eec07c863c74259b78fdba44d3846800273
Filip Wicher <filip.wicher@allegro.pl>
2020-12-28 18:07:31 +0100

    Contenerize

--- src/main/kotlin/com/github/lashu/foodideaprovider/homeFood/api/SwaggerRedirectEndpoint.kt
+++ src/main/kotlin/com/github/lashu/foodideaprovider/homeFood/api/SwaggerRedirectEndpoint.kt
@@ -1,16 +1,14 @@
 package com.github.lashu.foodideaprovider.homeFood.api
 
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import springfox.documentation.annotations.ApiIgnore
 
 @Controller
-@RequestMapping("/")
 class SwaggerRedirectEndpoint {
 
     @ApiIgnore
-    @GetMapping("/")
+    @RequestMapping
     fun swaggerUi(): String {
         return "redirect:/swagger-ui.html"
     }
